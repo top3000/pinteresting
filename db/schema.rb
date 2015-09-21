315260d41ede67f3622bc9517f2ce521ea50b49c
Victor Yee <victyee@gmail.com>
2015-09-21 14:35:37 +1000

    adds pins/users association

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,10 @@ ActiveRecord::Schema.define(version: 20150918094403) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
